59830dd2feff89b33d2a502f8edeffd6deb9204a
Khalidprithy <khalidprithy@gmail.com>
2022-03-19 01:09:31 +0600

    done

---
+++ src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import Popular from "../components/Popular";
+import Vegetable from "../components/Vegetable";
+import { motion } from "framer-motion";
+import React from "react";
+
+
+function Home() {
+    return (
+        <motion.div
+            animate={{ opacity: 1 }}
+            initial={{ opacity: 0 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.5 }}
+        >
+            <Vegetable></Vegetable>
+            <Popular></Popular>
+        </motion.div>
+    );
+}
+
+export default Home;
